1d4f2ea6a958083df7c44332d0e441aaccf92ba4
Pedro Sales Santos <psalessa@everis.com>
2020-08-29 21:00:29 -0300

    stock component

--- src/components/stocks/Stock.vue
+++ src/components/stocks/Stock.vue
@@ -1,5 +1,20 @@
 <template>
-  <h1>Stocks</h1>
+  <v-flex class="pr-3 pb-3" xs12 mg6 lg4>
+    <v-card class="green darken-3 white--text">
+      <v-card-title class="headline">
+        <strong>
+          Name
+          <small>(Price: Price)</small>
+        </strong>
+      </v-card-title>
+    </v-card>
+    <v-card>
+      <v-container fill-height>
+        <v-text-field label="Quantity" type="number" class="pr-2"></v-text-field>
+        <v-btn class="green darken-3 white--text">Buy</v-btn>
+      </v-container>
+    </v-card>
+  </v-flex>
 </template>
 
 <script>
